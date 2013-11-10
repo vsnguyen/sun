a1117b1925037650b66af045ddd6ac967fa4f1f1
Vietson Nguyen <vietson@gmail.com>
2013-11-09 23:58:38 -0500

    update db collection logics

--- routes/api/v1/post.js
+++ routes/api/v1/post.js
@@ -3,35 +3,54 @@
 */
 
 // MONGODB
-var MONGO = require('mongodb'),
-		DB = new MONGO.Db('sundb', new MONGO.Server('localhost',27017, {auto_reconnect:true}), {});
+var mongo = require('mongodb');
+var Server = mongo.Server,
+    Db = mongo.Db,
+    BSON = mongo.BSONPure;
 
-DB.open(function(err, db){
-	if(db){
-		console.log('Connected to db');
+var server = new Server('localhost', 27017, {auto_reconnect: true});
+var db = new Db('sundb', server);
 
-		db.collection('posts', function(err, collection){
-
-		})
-	}
-})
-
-// CRUD
+db.open(function(err, db) {
+  if(!err) {
+    console.log("Connected to 'subdb' database");
+    db.collection('posts', {strict:true}, function(err, collection) {
+      if (err) {
+        console.log("The 'posts' collection doesn't exist.");
+      }
+    });
+   }
+});
 
 exports.create = function(req, res){
-	res.send('create');
+  var post = req.body;
+  db.collection('posts', function(err, collection){
+    collection.insert(post, {safe: true}, function(err, result){
+      if(err){
+        res.send({'message':'An error has occurred', 'post': result[0], 'error': err});
+      } else {
+        console.log('Success: ' + JSON.stringify(result[0]));
+        res.send(result[0]);
+      }
+    });
+  });
+  res.send(collection);
 };
 
 exports.delete = function(req, res){
-	res.send('delete');
+  res.send('delete');
 };
 
 exports.update = function(req, res){
-	res.send('update');
+  res.send('update');
 };
 
 exports.index = function(req, res){
-	res.send('index');
+  db.collection('posts', function(err, collection) {
+    collection.find().toArray(function(err, items) {
+      res.send(items);
+    });
+  });
 };
 
 exports.show = function(req, res){
